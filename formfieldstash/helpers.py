77c5a5d6be3a05c297700fc85b886903d16357bd
benzkji <bnzk@bnzk.ch>
2019-11-15 09:19:07 +0100

    for the PEP8s sake

--- formfieldstash/helpers.py
+++ formfieldstash/helpers.py
@@ -7,9 +7,11 @@ def get_base_attrs(field_name):
     }
     return attrs
 
+
 def get_single_stash_attrs(field_name):
     attrs = get_base_attrs(field_name)
     return attrs
+
 
 def get_advanced_stash_attrs(field_name, stash_config):
     attrs = get_base_attrs(field_name)
